fb5ea922ea87de033e5a8d52b649a1777bd7fcaf
Александр Бахмутов <alexdmitralex@gmail.com>
2023-02-16 11:55:40 +0300

    Добавил сущность Местаположения

---
+++ src/main/java/com/example/if_else/Models/Location.java
@@ -0,0 +1,28 @@
+package com.example.if_else.Models;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "locations")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+
+}
